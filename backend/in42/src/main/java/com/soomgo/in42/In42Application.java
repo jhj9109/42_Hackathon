0ae0222b721a607d9cc6a348b79f09b126b0a86b
greatSweetMango <jhkim0097@gmail.com>
2023-03-15 16:58:24 +0900

    first commit

---
+++ backend/in42/src/main/java/com/soomgo/in42/In42Application.java
@@ -0,0 +1,13 @@
+package com.soomgo.in42;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class In42Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(In42Application.class, args);
+    }
+
+}
