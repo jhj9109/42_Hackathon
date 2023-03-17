abb1c61903f8c358ee0da248b976ef3d65501f15
greatSweetMango <jhkim0097@gmail.com>
2023-03-17 18:33:46 +0900

    [feat] API 매핑 기본 응답 작성

---
+++ backend/in42/src/main/java/com/soomgo/in42/domain/qustion/controller/QuestionControllerImpl.java
@@ -0,0 +1,39 @@
+package com.soomgo.in42.domain.qustion.controller;
+
+
+import com.soomgo.in42.domain.security.jwt.TokenService;
+import com.soomgo.in42.domain.user.dto.UserDto;
+import com.soomgo.in42.global.util.CookieUtil;
+import com.soomgo.in42.global.util.HeaderUtil;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping(value = "/question")
+public class QuestionControllerImpl {
+
+    private final TokenService tokenService;
+    @GetMapping(value = "")
+    public UserDto findOpenQuestion(HttpServletRequest request) {
+        UserDto user = tokenService.findUserByAccessToken(CookieUtil.getAccessToken(request), HeaderUtil.getAccessToken(request));
+        return user;
+    }
+
+    @PutMapping(value = "/{questionId}")
+    public UserDto CancelQuestion(HttpServletRequest request) {
+        UserDto user = tokenService.findUserByAccessToken(CookieUtil.getAccessToken(request), HeaderUtil.getAccessToken(request));
+        return user;
+    }
+
+    @PutMapping(value = "/{questionId}/answer")
+    public UserDto AnswerQuestion(HttpServletRequest request) {
+        UserDto user = tokenService.findUserByAccessToken(CookieUtil.getAccessToken(request), HeaderUtil.getAccessToken(request));
+        return user;
+    }
+}
