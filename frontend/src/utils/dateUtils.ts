9c0e4fa7bd1b0e49d30e4d9ead2dca35e32ed6e9
jhj9109 <jhj91_09@naver.com>
2023-03-20 03:07:20 +0900

    Update: 메인페이지 시간 지나면 "전"으로 표기

--- frontend/src/utils/dateUtils.ts
+++ frontend/src/utils/dateUtils.ts
@@ -1,17 +1,23 @@
 // 보통 형식 => "2023-03-18T10:00:00"
 export const getShortDate = (s: string) => s.slice(0, s.lastIndexOf(":")).replace("T", " ")
 export const getIntervalDateString = (targetDate: Date, currDate: Date) => {
+  let isElpased = false;
+  let intervalTime = targetDate.getTime() - currDate.getTime();
+  if (intervalTime <= 0) {
+    isElpased = true;
+    intervalTime *= -1;
+  }
   const intervals = {
-    days: Math.floor((targetDate.getTime() - currDate.getTime()) / (1000 * 60 * 60 * 24)),
-    hours: Math.floor((targetDate.getTime() - currDate.getTime()) / (1000 * 60 * 60)),
-    minutes: Math.floor((targetDate.getTime() - currDate.getTime()) / (1000 * 60)),
+    days: Math.floor(intervalTime / (1000 * 60 * 60 * 24)),
+    hours: Math.floor(intervalTime / (1000 * 60 * 60)),
+    minutes: Math.floor(intervalTime / (1000 * 60)),
   }
   if (intervals.days !== 0) {
-    return `${intervals.days}일 후`
+    return `${intervals.days}일 ${isElpased ? "전" : "후"}`
   } else if (intervals.hours !== 0 ) {
-    return `${intervals.hours}시간 후`
+    return `${intervals.hours}시간 ${isElpased ? "전" : "후"}`
   } else if (intervals.minutes) {
-    return `${intervals.minutes}분 후`
+    return `${intervals.minutes}분 ${isElpased ? "전" : "후"}`
   } else {
     return `now`;
   }
