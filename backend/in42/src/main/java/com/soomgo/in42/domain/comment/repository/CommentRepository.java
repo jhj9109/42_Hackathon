839a2cbbcea225cb716cd7a6006cdbd6a13635af
greatSweetMango <jhkim0097@gmail.com>
2023-03-19 22:04:23 +0900

    rmove

--- backend/in42/src/main/java/com/soomgo/in42/domain/comment/repository/CommentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.soomgo.in42.domain.comment.repository;
-
-import com.soomgo.in42.domain.comment.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Integer> {
-
-}
