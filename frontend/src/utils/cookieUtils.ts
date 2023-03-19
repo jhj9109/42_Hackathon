072b6a7ce7208ab88a5b148044ebd613f6beb17e
jhj9109 <jhj91_09@naver.com>
2023-03-20 08:34:07 +0900

    Update: https에서 변경 && 깃허브 주소 변경 && 푸터 색 제거

--- frontend/src/utils/cookieUtils.ts
+++ frontend/src/utils/cookieUtils.ts
@@ -1,4 +1,7 @@
+import { NEED_SECURE } from "../api/axios";
+
 export const ACCESS_TOKEN_STR = 'access_token';
+
 
 export const deleteCookie = (cookieName: string) => {
   document.cookie = `${cookieName}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
@@ -6,6 +9,10 @@ export const deleteCookie = (cookieName: string) => {
 export const mySetCookie = (cookieName: string, cookieValue: string) => {
   const expirationDate = new Date()
   expirationDate.setDate(expirationDate.getDate() + 30); // 30일 후 만료
-  const cookieString = `${cookieName}=${cookieValue};secure=true;expires=${expirationDate.toUTCString()};path=/`;
+  const cookieString =
+    NEED_SECURE
+    ? `${cookieName}=${cookieValue};secure=true;expires=${expirationDate.toUTCString()};path=/`
+    : `${cookieName}=${cookieValue};expires=${expirationDate.toUTCString()};path=/`;
+    
   document.cookie = cookieString;
 }
