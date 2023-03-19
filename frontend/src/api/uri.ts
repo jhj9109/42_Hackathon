78ef2045060d5dd74d610c7180496554b69afffc
jhj9109 <jhj91_09@naver.com>
2023-03-19 11:28:55 +0900

    Update: axios & uri 임시 코드

--- frontend/src/api/uri.ts
+++ frontend/src/api/uri.ts
@@ -1,3 +1,7 @@
 export const USER_SESSION_POST_PATH = "user/session";
+export const USER_SESSION_PATH = "user/session";
+export const USER_TAG_PATH = "user/tag";
+export const USER_QUESTION_PATH = "user/question";
+
 export const MENTEE_SLOTS_FIRST_STEP_URL = "/mentee/slots";
 export const MENTEE_SLOTS_NEXT_STEP_URL = "/mentee/slots/mentoring";
